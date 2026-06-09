c31aae0f3b92b18b0dda21668be474cd2d4a349e
JEAN EUGENIO CAMENT VALDERRAMA <je.cament@duocuc.cl>
2026-06-08 23:41:03 -0400

    avance en la ingesta

---
+++ carpetas/logging_utils/logging_utils.py
@@ -0,0 +1,42 @@
+import logging as lg
+import os 
+
+
+
+#esto permite crear un logger para registro de acciones en un archivo externo. Esto para cumplir con el principio 
+#de trazabilidad
+def configurar_logger(componente: str):
+    logger = lg.getLogger(f"Logger de {componente}")
+    logger.setLevel(lg.INFO)
+    logger.propagate = False
+
+    formato = lg.Formatter("| %(asctime)s --- %(levelname)s --- %(message)s |")
+    conshandler = lg.StreamHandler()
+
+    conshandler.setFormatter(formato)
+
+    os.makedirs("logs"  , exist_ok=True)
+
+    handler_archivo = lg.FileHandler(f"logs/logs de {componente}.log", encoding = "UTF-8", errors = "ignore", delay=True)
+    handler_archivo.setFormatter(formato)
+    logger.addHandler(handler_archivo)
+
+    return logger
+
+
+### asi se crea el logger, debes pasarle el nombre del componente que lo usa. para hacer uso, 
+# se puede hacer con los metodos .info, .error, .warning, .exception, .critical, a los cuales se les debe pasar el mensaje en string.
+logging_transformacion_limpieza = configurar_logger("limpieza_transformación")
+
+logging_ingesta = configurar_logger("ingesta")
+
+
+
+
+
+
+
+
+
+
+
--- carpetas/pipeline_ingesta/ingesta.py
+++ carpetas/pipeline_ingesta/ingesta.py
@@ -1,3 +1,55 @@
 import pandas as pd
+from pathlib import Path
+import logging_utils.logging_utils as lgu
 
-print("hola")
+extensiones_excel = [".xls", ".xlsx"]
+extensiones_csv = [".csv"]
+extensiones_simples = [".txt"]
+
+
+def leer_datos(path, extension, sheet_excel):
+    if extension in extensiones_excel:
+        try:
+           return  pd.read_excel(path, sheet_name= sheet_excel)
+        except Exception as e:
+           lgu.logging_ingesta.exception(f"Error de lectura: {e}")
+    elif extension in extensiones_csv:
+        try:
+            return pd.read_csv(path)
+        except Exception as e:
+            lgu.logging_ingesta.exception(f"Error de lectura: {e}")
+    elif extension in extensiones_simples:
+        try:
+           return pd.read_table(path)
+        except Exception as e:
+            lgu.logging_ingesta.exception(f"Error de lectura: {e}")
+    
+    else:
+        lgu.logging_ingesta.error(f'el archivo con la extensión "{extension}" '+
+                                       'no esta soportada por ahora en el pipeline.')
+        
+    
+
+
+
+#Funcion principal para la lectura de datos, sheet_excel se debe especificar si se trabaja con excel y 
+#y se requiere extraer la infromación de una hoja que no es la primera.
+#si devuelve none, quiere decir que fallo la lectura.
+def leer_archivo(direccion_archivo : str, sheet_excel = 0 ) -> pd.DataFrame:
+    f_path = Path(direccion_archivo)
+    lgu.logging_ingesta.info("iniciando lectura de datos.")
+    if(f_path.exists()):
+        extension = f_path.suffix
+
+        datos = leer_datos(direccion_archivo, extension, sheet_excel)
+    else:
+        print("no existe el archivo")
+        lgu.logging_ingesta.warning(f"Se ingreso la ruta de un archivo inexistente: {direccion_archivo}")
+
+    if(datos is not None):
+        lgu.logging_ingesta.info("[Finalizado] datos transformados en DataFrame")
+
+    return datos
+
+        
+
